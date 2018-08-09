0c2da5b6803e43b9a1d9a6224eb5c2886fa94a18
Danny Weinberg <danny@dropbox.com>
2018-08-09 12:22:17 -0700

    Build tutorial app.

--- hotslogs_mobile_client/lib/main.dart
+++ hotslogs_mobile_client/lib/main.dart
@@ -1,109 +1,86 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:hotslogs_mobile_client/saved_words.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: RandomWords(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+final biggerFont = const TextStyle(fontSize: 18.0);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to Flutter'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _buildSuggestions(),
     );
   }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, index) {
+          if (index.isOdd) {
+            return Divider();
+          }
+          final suggestionIndex = index ~/ 2;
+          while (index >= _suggestions.length) {
+            print("Creating index $suggestionIndex");
+            _suggestions.add(WordPair.random());
+          }
+          print("Building index $suggestionIndex");
+          return _buildRow(_suggestions[suggestionIndex]);
+        });
+  }
+
+  Widget _buildRow(WordPair wordPair) {
+    final alreadySaved = _saved.contains(wordPair);
+    return ListTile(
+      title: Text(wordPair.asPascalCase, style: biggerFont),
+      trailing: IconButton(
+          icon: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+          ),
+          onPressed: () {
+            setState(() {
+              if (alreadySaved) {
+                _saved.remove(wordPair);
+              } else {
+                _saved.add(wordPair);
+              }
+            });
+          }),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+          MaterialPageRoute<void>(
+            builder: (BuildContext context) => SavedWords(_saved),
+          ),
+        );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State createState() => RandomWordsState();
 }
---
+++ hotslogs_mobile_client/lib/saved_words.dart
@@ -0,0 +1,28 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:hotslogs_mobile_client/main.dart';
+
+class SavedWords extends StatelessWidget {
+  SavedWords(this._words);
+
+  final Set<WordPair> _words;
+
+  @override
+  Widget build(BuildContext context) {
+    final Iterable<ListTile> tiles = _words.map(
+      (pair) => ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: biggerFont,
+            ),
+          ),
+    );
+    final List<Widget> divided =
+        ListTile.divideTiles(tiles: tiles, context: context).toList();
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Saved Suggestions")),
+      body: ListView(children: divided),
+    );
+  }
+}
